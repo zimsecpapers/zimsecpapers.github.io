fc2e4328630c9699607697eb8c2e3b5da90b2806
zimsecpapers <farmplus93@gmail.com>
2023-03-19 10:39:26 +0100

    ads checker aded

---
+++ js/checker.js
@@ -0,0 +1,22 @@
+
+
+
+let adsWidth = document.getElementsByClassName('ads').offsetWidth;
+let isAdBlockAvailable = false;
+
+if (adsWidth == 0){
+    isAdBlockAvailable = true;
+	//alert("Adblocker detected. Please deactivate your adblocker");
+    const turnOffAddBlocker = confirm(
+      'Disable your adblocker so that ads can display if want to download resources here?',
+    );
+    if (turnOffAddBlocker) {
+        history.back();
+    }else{
+        history.back();
+    }
+} else {
+    isAdBlockAvailable = false;
+    // console.log(">> No adblocker present");
+    //console.log("user is null");
+}
